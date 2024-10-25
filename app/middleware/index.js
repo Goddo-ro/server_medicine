d2a6b11be8d82f0400ef46945dba1df1f9e09399
Goddo-ro <r0manovic1@yandex.ru>
2024-10-25 20:46:35 +0300

    add: middleware to transactions routes

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -1,4 +1,4 @@
-const { admin } = require("../config/firebase");
+const { admin } = require("../config/firebase.config");
 const verifyToken = async (req, res, next) => {
     const idToken = req.cookies.access_token;
     if (!idToken) {
--- app/routes/transaction.routes.js
+++ app/routes/transaction.routes.js
@@ -1,8 +1,9 @@
 module.exports = app => {
     const transaction = require("../controllers/transaction.controller");
+    const verifyToken = require('../middleware');
     const router = require("express").Router();
 
     router.get("/", transaction.findAll);
 
-    app.use("/api/transactions", router);
+    app.use("/api/transactions", verifyToken, router);
 }
